03babd75bfb12919ed7656898c8f2b147d221e12
ssalghanim <ssalghanim@ooredoo.qa>
2020-10-23 17:11:33 +0300

    try catch the lobby :)

--- FragmentServerWV_Core/LobbyChatRoom.cs
+++ FragmentServerWV_Core/LobbyChatRoom.cs
@@ -70,6 +70,8 @@ namespace FragmentServerWV
 
         public void DispatchPublicBroadcast(byte[] data, int who)
         {
+            try
+            {
             int id = FindRoomIndexById(who);
             byte[] temp = new byte[data.Length];
             data.CopyTo(temp, 0);
@@ -86,6 +88,12 @@ namespace FragmentServerWV
                     temp[1] = 0xff;
                     client.SendPacket30(0x7862, temp);
                 }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
         
         public void DispatchPrivateBroadcast(byte[] data, int who, int destid)
