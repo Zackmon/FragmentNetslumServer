9a20582ef7249aa231d7a781bfff0e4e14a39ea0
Zackmon <salemthunder.fist@gmail.com>
2020-10-24 15:48:13 +0300

    test

--- FragmentServerWV_WebApi/Models/PlayerModel.cs
+++ FragmentServerWV_WebApi/Models/PlayerModel.cs
@@ -21,6 +21,7 @@ namespace FragmentServerWV_WebApi.Models
         public int online_god_counter;
         public int offline_godCounter;
         
+        
         public static PlayerModel ConvertData(FragmentServerWV.GameClient client)
         {
             PlayerModel model = new PlayerModel();
