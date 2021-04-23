10db56e9eeb3b1be7d530f654742ef9134168c5f
Adam Sears <asears2@live.com>
2021-04-22 20:50:36 -0500

    #19 - Added a second opcode (which does nothing).

---
+++ FragmentServerWV_Core/Entities/OpCodeHandlers/Regular/OPCODE_KEY_EXCHANGE_ACKNOWLEDGMENT.cs
@@ -0,0 +1,16 @@
+﻿using FragmentServerWV.Entities.Attributes;
+using FragmentServerWV.Services.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace FragmentServerWV.Entities.OpCodeHandlers.Regular
+{
+    [OpCode(OpCodes.OPCODE_KEY_EXCHANGE_ACKNOWLEDGMENT)]
+    public sealed class OPCODE_KEY_EXCHANGE_ACKNOWLEDGMENT : IOpCodeHandler
+    {
+        public Task<ResponseContent> HandleIncomingRequestAsync(RequestContent request)
+        {
+            
+        }
+    }
+}
