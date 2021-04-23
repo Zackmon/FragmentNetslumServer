c2b0f3fcd2e2039c10a148a65e2532be6ea7d4fe
Adam Sears <asears2@live.com>
2021-04-22 21:06:34 -0500

    #19 - Created a base class that handles a default 'empty' response

---
+++ FragmentServerWV_Core/Entities/OpCodeHandlers/NoResponseOpCodeHandler.cs
@@ -0,0 +1,10 @@
+﻿using FragmentServerWV.Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace FragmentServerWV.Entities.OpCodeHandlers
+{
+    public abstract class NoResponseOpCodeHandler : IOpCodeHandler
+    {
+        public Task<ResponseContent> HandleIncomingRequestAsync(RequestContent request) => Task.FromResult(ResponseContent.Empty);
+    }
+}
