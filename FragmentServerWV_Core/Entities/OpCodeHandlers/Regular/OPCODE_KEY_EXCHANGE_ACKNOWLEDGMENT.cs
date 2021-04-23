75f03e89e868968ceb777bf6f8424e53e3e001a2
Adam Sears <asears2@live.com>
2021-04-22 21:06:52 -0500

    #19 - Use the NoResponseOpCodeHandler class in two handlers

--- FragmentServerWV_Core/Entities/OpCodeHandlers/Regular/OPCODE_KEY_EXCHANGE_ACKNOWLEDGMENT.cs
+++ FragmentServerWV_Core/Entities/OpCodeHandlers/Regular/OPCODE_KEY_EXCHANGE_ACKNOWLEDGMENT.cs
@@ -1,15 +1,10 @@
 ﻿using FragmentServerWV.Entities.Attributes;
-using FragmentServerWV.Services.Interfaces;
-using System.Threading.Tasks;
 
 namespace FragmentServerWV.Entities.OpCodeHandlers.Regular
 {
     [OpCode(OpCodes.OPCODE_KEY_EXCHANGE_ACKNOWLEDGMENT)]
-    public sealed class OPCODE_KEY_EXCHANGE_ACKNOWLEDGMENT : IOpCodeHandler
+    public sealed class OPCODE_KEY_EXCHANGE_ACKNOWLEDGMENT : NoResponseOpCodeHandler
     {
-        public Task<ResponseContent> HandleIncomingRequestAsync(RequestContent request)
-        {
-            return new Task<ResponseContent>(() => ResponseContent.Empty);
-        }
+        
     }
 }
---
+++ FragmentServerWV_Core/Entities/OpCodeHandlers/Regular/OPCODE_UNKNOWN.cs
@@ -0,0 +1,8 @@
+﻿using FragmentServerWV.Entities.Attributes;
+using System.ComponentModel;
+
+namespace FragmentServerWV.Entities.OpCodeHandlers.Regular
+{
+    [OpCode(0x0002), DisplayName("Unknown Handler"), Description("Handles an opcode that seemingly has no function")]
+    public sealed class OPCODE_UNKNOWN : NoResponseOpCodeHandler { }
+}
