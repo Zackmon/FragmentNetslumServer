64c7392d1d41f0f3b5739d62e5d5ac3b9d5131eb
Adam Sears <asears2@live.com>
2021-04-23 02:39:01 -0500

    #19 - Created the SimpleResponseOpCodeHandler which streamlines submitting SIMPLE responses back to the client, which is what most opcodes do

---
+++ FragmentServerWV_Core/Entities/OpCodeHandlers/SimpleResponseOpCodeHandler.cs
@@ -0,0 +1,21 @@
+﻿using FragmentServerWV.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FragmentServerWV.Entities.OpCodeHandlers
+{
+    public abstract class SimpleResponseOpCodeHandler : IOpCodeHandler
+    {
+        private readonly ushort responseOpCode;
+        private readonly byte[] responseData;
+
+        public SimpleResponseOpCodeHandler(ushort responseOpCode, byte[] responseData)
+        {
+            this.responseOpCode = responseOpCode;
+            this.responseData = responseData;
+        }
+
+        public virtual Task<IEnumerable<ResponseContent>> HandleIncomingRequestAsync(RequestContent request) =>
+            Task.FromResult<IEnumerable<ResponseContent>>(new[] { request.CreateResponse(responseOpCode, responseData) });
+    }
+}
