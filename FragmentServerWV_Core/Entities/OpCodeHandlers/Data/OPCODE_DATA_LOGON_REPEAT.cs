41652824da8ff4762a03cfbe0bfa800e5edb4916
Adam Sears <asears2@live.com>
2021-04-23 02:39:31 -0500

    #19 - Use SimpleResponseOpCodeHandler for OPCODE_DATA_LOGON_REPEAT

--- FragmentServerWV_Core/Entities/OpCodeHandlers/Data/OPCODE_DATA_LOGON_REPEAT.cs
+++ FragmentServerWV_Core/Entities/OpCodeHandlers/Data/OPCODE_DATA_LOGON_REPEAT.cs
@@ -1,18 +1,10 @@
 ﻿using FragmentServerWV.Entities.Attributes;
-using FragmentServerWV.Services.Interfaces;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace FragmentServerWV.Entities.OpCodeHandlers.Data
 {
     [OpCodeData(OpCodes.OPCODE_DATA_LOGON_REPEAT)]
-    public sealed class OPCODE_DATA_LOGON_REPEAT : IOpCodeHandler
+    public sealed class OPCODE_DATA_LOGON_REPEAT : SimpleResponseOpCodeHandler
     {
-        public Task<IEnumerable<ResponseContent>> HandleIncomingRequestAsync(RequestContent request) =>
-            Task.FromResult<IEnumerable<ResponseContent>>(new[] { new ResponseContent(
-                request,
-                OpCodes.OPCODE_DATA_LOGON_RESPONSE,
-                new byte[] { 0x02, 0x10 },
-                null)});
+        public OPCODE_DATA_LOGON_REPEAT() : base(OpCodes.OPCODE_DATA_LOGON_RESPONSE, new byte[] { 0x02, 0x10 }) { }
     }
 }
