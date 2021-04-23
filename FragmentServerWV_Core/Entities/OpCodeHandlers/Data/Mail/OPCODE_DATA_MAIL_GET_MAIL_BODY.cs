72870b1b72ca2de354c422049adad01f13eb5ad8
Adam Sears <asears2@live.com>
2021-04-23 03:13:28 -0500

    #19 - Wave two of new OPCODEs converted

---
+++ FragmentServerWV_Core/Entities/OpCodeHandlers/Data/Mail/OPCODE_DATA_MAIL_GET_MAIL_BODY.cs
@@ -0,0 +1,29 @@
+﻿using FragmentServerWV.Entities.Attributes;
+using FragmentServerWV.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using static FragmentServerWV.Services.Extensions;
+
+namespace FragmentServerWV.Entities.OpCodeHandlers.Data.Mail
+{
+    [OpCodeData(OpCodes.OPCODE_DATA_MAIL_GET_MAIL_BODY), Description("Returns the full message body for the specified mail")]
+    public sealed class OPCODE_DATA_MAIL_GET_MAIL_BODY : IOpCodeHandler
+    {
+        private readonly IMailService mailService;
+
+        public OPCODE_DATA_MAIL_GET_MAIL_BODY(IMailService mailService)
+        {
+            this.mailService = mailService ?? throw new ArgumentNullException(nameof(mailService));
+        }
+
+        public async Task<IEnumerable<ResponseContent>> HandleIncomingRequestAsync(RequestContent request)
+        {
+            var mailId = (int)swap32(BitConverter.ToUInt32(request.Data, 4));
+            var messageBodyModel = await mailService.GetMailContent(mailId);
+            var messageBody = await mailService.ConvertMailBodyIntoBytes(messageBodyModel);
+            return new[] { request.CreateResponse(OpCodes.OPCODE_DATA_MAIL_GET_MAIL_BODY_RESPONSE, messageBody) };
+        }
+    }
+}
---
+++ FragmentServerWV_Core/Entities/OpCodeHandlers/Data/OPCODE_DATA_0X771E.cs
@@ -0,0 +1,10 @@
+﻿using FragmentServerWV.Entities.Attributes;
+
+namespace FragmentServerWV.Entities.OpCodeHandlers.Data
+{
+    [OpCodeData(0x771E)]
+    public sealed class OPCODE_DATA_0X771E : SimpleResponseOpCodeHandler
+    {
+        public OPCODE_DATA_0X771E() : base(0x771F, new byte[] { 0x00, 0x00 }) { }
+    }
+}
