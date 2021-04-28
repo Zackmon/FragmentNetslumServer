dfe83f228e544d14a977694b868f046d2ee7be01
Adam Sears <asears2@live.com>
2021-04-27 19:07:42 -0500

    #19 - The acknowledgement handler needs to initialize the keys correctly...

--- FragmentServerWV_Core/Entities/OpCodeHandlers/Regular/OPCODE_KEY_EXCHANGE_ACKNOWLEDGMENT.cs
+++ FragmentServerWV_Core/Entities/OpCodeHandlers/Regular/OPCODE_KEY_EXCHANGE_ACKNOWLEDGMENT.cs
@@ -1,7 +1,17 @@
 ﻿using FragmentServerWV.Entities.Attributes;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace FragmentServerWV.Entities.OpCodeHandlers.Regular
 {
     [OpCode(OpCodes.OPCODE_KEY_EXCHANGE_ACKNOWLEDGMENT)]
-    public sealed class OPCODE_KEY_EXCHANGE_ACKNOWLEDGMENT : NoResponseOpCodeHandler { }
+    public sealed class OPCODE_KEY_EXCHANGE_ACKNOWLEDGMENT : NoResponseOpCodeHandler
+    {
+        public override Task<IEnumerable<ResponseContent>> HandleIncomingRequestAsync(RequestContent request)
+        {
+            request.Client.InitializeDecryptionKey(request.Client.from_key, true);
+            request.Client.InitializeEncryptionKey(request.Client.to_key, true);
+            return base.HandleIncomingRequestAsync(request);
+        }
+    }
 }
