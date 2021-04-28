bc511f8dd55f33679243f3549b312f631bc18316
Adam Sears <asears2@live.com>
2021-04-27 19:07:20 -0500

    #19 - I forgot to add the attribute for handling return to desktop!

--- FragmentServerWV_Core/Entities/OpCodeHandlers/Data/OPCODE_DATA_RETURN_DESKTOP.cs
+++ FragmentServerWV_Core/Entities/OpCodeHandlers/Data/OPCODE_DATA_RETURN_DESKTOP.cs
@@ -1,9 +1,11 @@
-﻿using FragmentServerWV.Services;
+﻿using FragmentServerWV.Entities.Attributes;
+using FragmentServerWV.Services;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace FragmentServerWV.Entities.OpCodeHandlers.Data
 {
+    [OpCodeData(OpCodes.OPCODE_DATA_RETURN_DESKTOP)]
     public sealed class OPCODE_DATA_RETURN_DESKTOP : SimpleResponseOpCodeHandler
     {
         public OPCODE_DATA_RETURN_DESKTOP() : base(OpCodes.OPCODE_DATA_RETURN_DESKTOP_OK, new byte[] { 0x00, 0x00 }) { }
